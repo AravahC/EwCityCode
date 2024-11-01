ebb7d0dd954380fa18a5dad60cb1bc21090c77fb
AravahC <147439577+AravahC@users.noreply.github.com>
2024-11-01 18:04:19 -0400

    Update DetectCollisions.cs

--- DetectCollisions.cs
+++ DetectCollisions.cs
@@ -18,25 +18,25 @@ public class DetectCollisions : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        //if it hits the front plane ends the game
+        //if the fly hits the front plane ends the game
         if (other.gameObject.CompareTag("FrontPlane"))
         {
             Destroy(gameObject);
             gameManager.GameOver();
         }
-        //if it hits the back plane gets destroyed
+        //if the fly hits the back plane gets destroyed
         else if (other.gameObject.CompareTag("BackPlane"))
         {
             Destroy(gameObject);
         }
-        //if it gets hit by a fireball score increases+gets destroyed
+        //if the fly gets hit by a fireball score increases+gets destroyed
         else if (other.gameObject.CompareTag("Fire"))
         {
             gameManager.UpdateScore(10);
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
-        //if it hits the player score decreases+destroyed
+        //if the fly hits the player score decreases+destroyed
         else if (other.gameObject.CompareTag("Player")) { 
             Destroy(gameObject);
             gameManager.UpdateScore(-5);
